ac68cbb202c8c8dc388e3d5841df000f64a68849
adevmir <ademirvalonqar@gmail.com>
2022-08-23 14:31:56 -0300

    [Correção requisito 01]

--- app.js
+++ app.js
@@ -2,16 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const products = require('./controllers/products');
-
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
-app.get('/products', products.getAll);
-
-app.get('/products/:id', products.getById);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = require('./app');
 require('dotenv').config();
 
+const products = require('./controllers/products');
+
+app.get('/products/:id', products.getById);
+app.get('/products', products.getAll);
+
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
